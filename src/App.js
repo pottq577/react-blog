291583211de4e780e55e67320d0b78e6c7731421
HWKIM <pottq577@naver.com>
2023-03-26 17:16:01 +0900

    using component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function App() {
   ]);
   let [추천, 추천변경] = useState(0);
   let posts = "강남고기맛집";
+  // function 제목바꾸기() {
+  //   var newArray = [...글제목];
+  //   newArray[0] = "여자코트 추천";
+  //   글제목변경(newArray);
+  // }
   return (
     <div className="App">
       <div className="black-nav">
@@ -36,7 +41,33 @@ function App() {
         <p>02/19</p>
         <hr></hr>
       </div>
+      <Modal></Modal>
     </div>
+  );
+}
+
+/**
+ * ! 유의사항
+ * 이름은 대문자
+ * return 안에 있는건 태그 하나여야만 함, 추가하고싶으면 <></> 안에
+ * ! 컴포넌트를 만드는 기준
+ * 반복출현하는 덩어리들
+ * 자주 변경되는 HTML UI들
+ * 다른 페이지 만들 때
+ * ! 단점
+ * state 쓸 때 복잡함 (props문법 이용하면 됨)
+ */
+
+function Modal() {
+  return (
+    <>
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+      <div></div>
+    </>
   );
 }
 
