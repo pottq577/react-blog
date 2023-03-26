ccb2be529719959397c6a1e7c4203b8c1b0e2cdb
HWKIM <pottq577@naver.com>
2023-03-26 15:43:20 +0900

    initial code setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import React, { useState } from "react";
 
 function App() {
+  let [글제목, 글제목변경] = useState([
+    "남자코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
+  let posts = "강남고기맛집";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{글제목[0]}</h3>
+        <p>02/17</p>
+        <hr></hr>
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>02/18</p>
+        <hr></hr>
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>02/19</p>
+        <hr></hr>
+      </div>
     </div>
   );
 }
