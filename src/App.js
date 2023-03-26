82c717bef6948984a033823557ddd6fce75e84f8
HWKIM <pottq577@naver.com>
2023-03-26 16:01:28 +0900

    handle event

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import "./App.css";
 import logo from "./logo.svg";
 import React, { useState } from "react";
@@ -8,6 +10,7 @@ function App() {
     "강남 우동맛집",
     "파이썬독학",
   ]);
+  let [추천, 추천변경] = useState(0);
   let posts = "강남고기맛집";
   return (
     <div className="App">
@@ -15,7 +18,11 @@ function App() {
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h3>{글제목[0]}</h3>
+        <h3>
+          {글제목[0]}
+          <span onClick={() => 추천변경(추천 + 1)}>👍</span>
+          {추천}
+        </h3>
         <p>02/17</p>
         <hr></hr>
       </div>
